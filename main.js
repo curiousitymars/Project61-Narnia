c272736d5e91166dadae13ae582d2cf0dbd7313b
curiousitymars <84633190+curiousitymars@users.noreply.github.com>
2021-07-06 12:27:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function flipimage() {
+document.getElementById("image1"). src="Peter's flipcard.jpg"
+}
+function flipimage() {
+ document.getElementById("image2"). src="Jadis' flipcard.JPG"   
+}
+function flipimage() {
+document.getElementById("image3"). src="Prince Caspian's flipcard.JPG"
+}
